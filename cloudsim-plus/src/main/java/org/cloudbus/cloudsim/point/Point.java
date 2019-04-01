0f252c863116b321f571eeea4dbf7a87e6f24f05
Nikolaos <49083436+Nicklatzwn@users.noreply.github.com>
2019-04-01 03:09:48 +0300

    Update Point.java

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/point/Point.java
+++ cloudsim-plus/src/main/java/org/cloudbus/cloudsim/point/Point.java
@@ -1,10 +1,15 @@
 package org.cloudbus.cloudsim.point;
+
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Point {
 	
 	private double x;
 	private double y;
+	private List<Integer> coordinates_x;
+	private List<Integer> coordinates_y;
 	
 	private double speed_x;
 	private double speed_y;
@@ -17,6 +22,14 @@ public class Point {
 	public Point(double x,double y) {
 		this.x=x;
 		this.y=y;
+		coordinates_x=new ArrayList<Integer>();
+		coordinates_y=new ArrayList<Integer>();
+		long longValue_x = Math.round(x);
+		long longValue_y = Math.round(y);
+		int intValue_x = (int) longValue_x;
+		int intValue_y = (int) longValue_y;
+		coordinates_x.add(intValue_x);
+		coordinates_y.add(intValue_y);
 	}
 	
 	public Point() {
@@ -32,38 +45,41 @@ public class Point {
 	}
 	public void setxPoint(double x) {
 		this.x=x;
+		long longValue_x = Math.round(x);
+		int intValue_x = (int) longValue_x;
+		coordinates_x.add(intValue_x);
 	}
 	public void setyPoint(double y) {
 		this.y=y;
+		long longValue_y = Math.round(y);
+		int intValue_y = (int) longValue_y;
+		coordinates_y.add(intValue_y);
 	}
 	public void move(double dt) {
 		
 		double x_new=x + speed_x*dt;
 		double y_new=y + speed_y*dt;
 		
-		setxPoint(x_new);
-		setyPoint(y_new);
-		
 		if(x_new>limit) {
-			setxPoint(limit-range);
+			x_new=limit-range;
 			speed_x*=-1;
 		}
-		
-			
+	
 		else if(x_new<0) {
-				setxPoint(range);
+			    x_new=range;
 				speed_x*=-1;
 			}
 		if(y_new>limit) {
-			setyPoint(limit-range);
+			y_new=limit-range;
 			speed_y*=-1;
 		}	
 			
 		else if(y_new<0) {
-			setyPoint(range);
+			y_new=range;
 			speed_y*=-1;
 		}
-
+		setxPoint(x_new);
+		setyPoint(y_new);
 	}
 
 	public void set_speed(double speed_x,double speed_y) {
@@ -78,5 +94,11 @@ public class Point {
 	}
 	public double get_speed_y() {
 		return speed_y;
+	}
+	public List<Integer> get_coordinates_List_x() {
+		return coordinates_x;
+	}
+	public List<Integer> get_coordinates_List_y() {
+		return coordinates_y;
 	}
 }
