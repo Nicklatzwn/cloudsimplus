1bcffc4d965c4e63ad3bee5dc64ad1c68f1555b2
Nikolaos <49083436+Nicklatzwn@users.noreply.github.com>
2019-04-12 16:42:36 +0300

    Update Cloud_Servers_Info.java

--- cloudsim-plus-examples/src/main/java/nick/Cloud_Servers_Info.java
+++ cloudsim-plus-examples/src/main/java/nick/Cloud_Servers_Info.java
@@ -1,17 +1,21 @@
 package nick;
 
+import org.cloudbus.cloudsim.cloudlets.Cloudlet;
 import org.cloudbus.cloudsim.datacenters.Datacenter;
+import java.util.*;
 
 public class Cloud_Servers_Info extends Device_Info {
 
 	private int upload_speed;
 	private int download_speed;
+	private List<Cloudlet> buffer;
 	
 	public Cloud_Servers_Info(Datacenter datacenter,int upload_speed,int download_speed) {
 		super(datacenter);
 		// TODO Auto-generated constructor stub
 		this.upload_speed=upload_speed;
 		this.download_speed=download_speed;
+		buffer=new ArrayList<Cloudlet>();
 	}
 	
 	public int get_upload_speed() {
@@ -19,7 +23,16 @@ public class Cloud_Servers_Info extends Device_Info {
 	}
 	public int get_download_speed() {
 		return download_speed;
-	}	
+	}
+	public void add_to_buffer(List<Cloudlet> cloudlets) {
+		buffer.addAll(cloudlets);
+	}
+	public List<Cloudlet> get_buffer_list() {
+		return buffer;
+	}
+	public void clear_buffer() {
+		buffer.clear();
+	}
 	public void execute_the_cloudlets_from_mobile_to_main_Cloud_Server(Mobiles_Info info) {
 		super.getBroker().submitCloudletList(info.get_the_list_of_cloudlets_that_are_going_to_be_submitted());
 	}	
