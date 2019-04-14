cfe5359eaeba673c9ebe0ab05fbb09f9fcbfd043
Nikolaos <49083436+Nicklatzwn@users.noreply.github.com>
2019-04-15 01:10:27 +0300

    Update Mobiles_Info.java

--- cloudsim-plus-examples/src/main/java/nick/Mobiles_Info.java
+++ cloudsim-plus-examples/src/main/java/nick/Mobiles_Info.java
@@ -225,10 +225,10 @@ public class Mobiles_Info extends Device_Info{
 		b=(40+mob_id*50)%255;
 		List<Plotter> windowPlots = new ArrayList<>();
 		for(int i=0; i<CPU_SERIES.size(); i++) {
-		Plotter cpuWin = new Plotter(String.format("Energy_CPU of the Mobile:%d for Host:%d with total energy consumption: %.0f Watt (%.5f KWatt-Hour) and Mean %.4f Watt-Sec",mob_id,i, super.getTotalPowerForTheHost(i),PowerAware.wattsSecToKWattsHour(super.getTotalPowerForTheHost(i)),super.getTotalPowerForTheHost(i)/time), CPU_SERIES.get(i),new Color(r,g,b));
+		Plotter cpuWin = new Plotter(String.format("Energy_CPU of the Mobile:%d for Host:%d with total energy consumption: %.0f Watt-Sec (%.5f KWatt-Hour) and Mean %.4f Watt-Sec",mob_id,i, super.getTotalPowerForTheHost(i),PowerAware.wattsSecToKWattsHour(super.getTotalPowerForTheHost(i)),super.getTotalPowerForTheHost(i)/time), CPU_SERIES.get(i),new Color(r,g,b));
 		windowPlots.add(cpuWin);
 		}
-		Plotter wifiWin = new Plotter(String.format("Energy_WIFI of the Mobile:%d with total energy consumption: %6.4f Watt",mob_id,energy_for_Wifi_module/time), WIFI_SERIES,new Color(r,g,b));
+		Plotter wifiWin = new Plotter(String.format("Energy_WIFI of the Mobile:%d with total energy consumption: %6.4f Joules",mob_id,energy_for_Wifi_module), WIFI_SERIES,new Color(r,g,b));
 		Plotter EWin = new Plotter(String.format("Energy_3G of the Mobile:%d with total energy Sum:%6.2f Watt",mob_id,energy_for_3g_module), E_SERIES,new Color(r,g,b));
 		Plotter BatteryWin = new Plotter(String.format("The Battery of the Mobile:%d",mob_id), BATTERY_SERIES,new Color(r,g,b));
 		List<Double> ΑverageResponseTime_List = get_the_ΑverageResponseTime_List();
