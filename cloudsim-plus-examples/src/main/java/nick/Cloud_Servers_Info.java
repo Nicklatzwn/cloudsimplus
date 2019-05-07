e9a8b5e96ac7bf8b0eb555b0ff05f17fe9aea88f
Nikolaos <49083436+Nicklatzwn@users.noreply.github.com>
2019-05-07 03:52:39 +0300

    Update Cloud_Servers_Info.java

--- cloudsim-plus-examples/src/main/java/nick/Cloud_Servers_Info.java
+++ cloudsim-plus-examples/src/main/java/nick/Cloud_Servers_Info.java
@@ -10,7 +10,6 @@ public class Cloud_Servers_Info extends Device_Info {
 	private int download_speed;
 	private List<Cloudlet> buffer;
 	private List<Integer> Cloudlets_for_Mobiles;
-	private List<Cloudlet> Cloudlets;
 	
 	public Cloud_Servers_Info(Datacenter datacenter,int upload_speed,int download_speed) {
 		super(datacenter);
@@ -19,28 +18,33 @@ public class Cloud_Servers_Info extends Device_Info {
 		this.download_speed=download_speed;
 		buffer=new ArrayList<Cloudlet>();
 		Cloudlets_for_Mobiles = new ArrayList<Integer>();
-		Cloudlets=new ArrayList<Cloudlet>();
 	}
 	
 	public int get_upload_speed() {
 		return upload_speed;
 	}
+	
 	public int get_download_speed() {
 		return download_speed;
 	}
+	
 	public void add_to_buffer(List<Cloudlet> cloudlets) {
 		buffer.addAll(cloudlets);
 	}
+	
 	public List<Cloudlet> get_buffer_list() {
 		return buffer;
 	}
+	
 	public void clear_buffer() {
 		buffer.clear();
 	}
+	
 	public void execute_the_cloudlets_from_mobile_to_main_Cloud_Server(Mobiles_Info info) {
 		super.getBroker().submitCloudletList(info.get_the_list_of_cloudlets_that_are_going_to_be_submitted());
 		add_to_Cloudlets_for_Mobiles(info.get_mob_id(),info.get_the_list_of_cloudlets_that_are_going_to_be_submitted());
 	}
+	
 	public void add_to_Cloudlets_for_Mobiles(int mob_id,List<Cloudlet> cloudlets) {
 		Cloudlets.addAll(cloudlets);
 		for(int i=0; i<cloudlets.size(); i++) Cloudlets_for_Mobiles.add(mob_id);
